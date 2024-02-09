83e2abb6b17beefb816560c475649ca31bd6c30a
pw-02 <watters.patrick@outlook.com>
2024-02-09 01:31:40 +0000

    classification/configs/imagenet-resnet18-torch.yaml

--- launch.py
+++ launch.py
@@ -108,7 +108,7 @@ if __name__ == "__main__":
     torch.set_float32_matmul_precision("high")
 
     defaults = {
-       "config_file": 'classification/configs/imagenet-torch-config.yaml',
+       "config_file": 'classification/configs/imagenet-resnet18-torch.yaml',
         # "config_file": 'language/configs/gpt2-classic-pytorch.yaml',
         # "config_file": 'configs/exp1/resnet_resnet18_super.yaml',
 
