f4ae7d4fe5f34d41b615d38682dc9971987f427b
pw-02 <watters.patrick@outlook.com>
2024-02-23 18:38:17 +0000

    pythia-6.9b

--- mlworklaods/language/pretrain/openwebtext.py
+++ mlworklaods/language/pretrain/openwebtext.py
@@ -25,7 +25,7 @@ from lit_gpt.utils import CLI, chunked_cross_entropy, estimate_flops, get_defaul
 
 
 def setup(
-    model_name: str = "pythia-2.8b",
+    model_name: str = "pythia-6.9b",
     precision: Optional[str] = None,
     resume: Union[bool, Path] = False,
     devices: int = 1,
