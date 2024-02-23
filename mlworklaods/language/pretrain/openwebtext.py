fe88e7c7e828fd39cc221892c82003c26d0b665f
pw-02 <watters.patrick@outlook.com>
2024-02-23 18:30:56 +0000

    open_llama_7b

--- mlworklaods/language/pretrain/openwebtext.py
+++ mlworklaods/language/pretrain/openwebtext.py
@@ -25,7 +25,7 @@ from lit_gpt.utils import CLI, chunked_cross_entropy, estimate_flops, get_defaul
 
 
 def setup(
-    model_name: str = "open_llama_3b",
+    model_name: str = "open_llama_7b",
     precision: Optional[str] = None,
     resume: Union[bool, Path] = False,
     devices: int = 1,
