2ee8944f2d9e12be3639a4aa2c0a69f0b5fd8105
pw-02 <watters.patrick@outlook.com>
2024-03-01 19:17:10 +0000

    devices: int = 4

--- mlworklaods/language/pretrain/openwebtext.py
+++ mlworklaods/language/pretrain/openwebtext.py
@@ -30,7 +30,7 @@ def setup(
     precision: Optional[str] = None,
     resume: Union[bool, Path] = False,
     seed: int = 1337,
-    devices: int = 1,
+    devices: int = 4,
     io: IOArgs = IOArgs(train_data_dir=Path("data/openwebtext"), val_data_dir=None, out_dir=Path("out/openwebtext")),
     train: TrainArgs = TrainArgs(
         save_interval=1000,
