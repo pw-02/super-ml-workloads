2c9d146de605dd9169b595ca6a135218b46923f5
pw-02 <watters.patrick@outlook.com>
2024-04-19 02:34:29 +0000

    cifar10

--- mlworklaods/image_classification/train_image_classifer.py
+++ mlworklaods/image_classification/train_image_classifer.py
@@ -371,7 +371,7 @@ def transform():
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     # transformations = transforms.Compose([torchvision.transforms.ToTensor(), normalize])
     transformations = transforms.Compose([
-                transforms.RandomResizedCrop(224),
+                # transforms.RandomResizedCrop(224),
                 # transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
                 normalize,
