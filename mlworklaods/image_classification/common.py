77779946fff4d176e465273179391d53f979d10e
pw-02 <watters.patrick@outlook.com>
2024-05-11 17:01:56 +0000

    imagemey transformation added

--- mlworklaods/image_classification/common.py
+++ mlworklaods/image_classification/common.py
@@ -12,15 +12,28 @@ def make_model(fabric: Fabric, model_name: str):
 
 
 # Transformation function for data augmentation
+
 def transform():
     normalize = transforms.Normalize(
         mean=[0.485, 0.456, 0.406], 
         std=[0.229, 0.224, 0.225],
     )
     return transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         normalize,
     ])
+
+# def transform():
+#     normalize = transforms.Normalize(
+#         mean=[0.485, 0.456, 0.406], 
+#         std=[0.229, 0.224, 0.225],
+#     )
+#     return transforms.Compose([
+#         transforms.ToTensor(),
+#         normalize,
+#     ])
 
 # Calculate accuracy for top-k predictions
 def accuracy(output: Tensor, target: Tensor, topk=(1,)):
