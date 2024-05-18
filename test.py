c3782d00dedd24f85f49472ee12396e629926f75
pw-02 <watters.patrick@outlook.com>
2024-05-18 18:15:45 +0000

    c1

--- test.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-tensor = torch.tensor([2, 2, 7, 6, 3, 4, 8, 1, 6, 8, 4, 9, 3, 1, 6, 9, 9, 7, 4, 6, 1, 0, 3, 0])
-unique_values = torch.unique(tensor)
-num_unique_values = unique_values.numel()
-
-print(f'The number of unique values is: {num_unique_values}')
