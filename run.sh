d55d1c6cc39840d589b5e4a26c890ef54a615838
pw-02 <watters.patrick@outlook.com>
2024-05-22 19:38:22 +0000

    run

--- run.sh
+++ run.sh
@@ -1,3 +1,17 @@
+# #!/bin/bash
+
+# # Start the first Python script and run it in the background
+
+
+# python mlworklaods/run.py config.training.learning_rate=0.01
+
+# # Start the second Python script and run it in the background
+# # python mlworklaods/image_classification/launch.py &
+
+# # Wait for all background processes to finish
+# wait
+
+
 #!/bin/bash
 
 # Define an array of GPU indices
@@ -10,7 +24,7 @@ learning_rates=(0.001 0.01 0.1 1.0)
 for i in "${!gpu_indices[@]}"; do
     gpu_index=${gpu_indices[$i]}
     lr=${learning_rates[$i]}
-    CUDA_VISIBLE_DEVICES="$gpu_index" python mlworklaods/run.py --learning-rate "$lr" &
+    CUDA_VISIBLE_DEVICES="$gpu_index" python mlworklaods/run.py config.training.learning_rate="$lr" &
     sleep 1  # Wait for 1 second
 done
 
