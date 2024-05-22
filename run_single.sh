3d95470b47e52e255d0b1b13d26a35c30da85972
pw-02 <watters.patrick@outlook.com>
2024-05-22 20:15:54 +0000

    lets see

--- run_single.sh
+++ run_single.sh
@@ -13,18 +13,22 @@
 
 
 #!/bin/bash
+current_datetime=$(date +"%Y-%m-%d_%H-%M-%S")
+expid="single_job_$current_datetime"
 
 # Define an array of GPU indices
-gpu_indices=(0 1 2 3)
+gpu_indices=(0)
 
 # Define an array of learning rates
-learning_rates=(0.001 0.01 0.1 1.0)
+learning_rates=(0.001)
 
 # Loop over each job
 for i in "${!gpu_indices[@]}"; do
+    echo "$expid"
+
     gpu_index=${gpu_indices[$i]}
     lr=${learning_rates[$i]}
-    CUDA_VISIBLE_DEVICES="$gpu_index" python mlworklaods/run.py training.learning_rate="$lr" exp_id=multi_job job_id="$gpu_index" &
+    CUDA_VISIBLE_DEVICES="$gpu_index" python mlworklaods/run.py training.learning_rate="$lr" exp_id="$expid" job_id="$gpu_index" &
     sleep 1  # Wait for 1 second
 done
 
