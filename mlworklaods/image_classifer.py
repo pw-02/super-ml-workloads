7441e455ffd28a9cb12db232c66af83e30153605
pw-02 <watters.patrick@outlook.com>
2024-08-20 22:56:46 -0700

    logging resolved

--- mlworklaods/image_classifer.py
+++ mlworklaods/image_classifer.py
@@ -16,14 +16,20 @@ import os
 from collections import OrderedDict
 import numpy as np
 from resource_monitor import ResourceMonitor
-  
+import datetime
+
 def train_image_classifer(config: DictConfig):
     # Initialize TorchFabric
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    log_dir = f"{config.log_dir}/{config.workload.name.replace('/','_')}/{timestamp}".lower()
+    os.makedirs(log_dir, exist_ok=True)
+    train_logger = CSVLogger(root_dir=log_dir, name="train", prefix='', flush_logs_every_n_steps=config.log_interval)
+    val_logger = CSVLogger(root_dir=log_dir, name="val", prefix='', flush_logs_every_n_steps=config.log_interval)
+    
     fabric = Fabric(
         accelerator=config.accelerator, 
         devices=config.devices, 
-        precision=config.workload.precision,
-        loggers=[CSVLogger(root_dir=config.log_dir, name="", flush_logs_every_n_steps=config.log_interval)]    
+        precision=config.workload.precision   
     )
 
     seed_everything(config.seed) # instead of torch.manual_seed(...)
@@ -52,10 +58,11 @@ def train_image_classifer(config: DictConfig):
                 )
             train_dataloader = DataLoader(train_dataset, batch_size=None, sampler=train_sampler, num_workers=config.workload.num_pytorch_workers)
             train_dataloader = fabric.setup_dataloaders(train_dataloader)
+        
         if config.workload.run_validation:
             val_dataset = S3MappedDataset(s3_bucket=config.workload.s3_bucket, s3_prefix=config.workload.s3_val_prefix, transform=val_transform)
             val_sampler = BatchSamplerWithID(
-                sampler= SequentialSampler(data_source=train_dataset),
+                sampler= SequentialSampler(data_source=val_dataset),
                 batch_size=config.workload.batch_size, 
                 drop_last=False
                 )
@@ -64,9 +71,10 @@ def train_image_classifer(config: DictConfig):
             val_dataloader = fabric.setup_dataloaders(val_dataloader)
 
     # Start training
-    metric_collector = ResourceMonitor(interval=1, flush_interval=10, file_path= f'{ fabric.loggers[0].log_dir}/resource_usage_metrics.json')
+    metric_collector = ResourceMonitor(interval=1, flush_interval=10, file_path= f'{log_dir}/resource_usage_metrics.json')
     metric_collector.start()
-    global_step_count = 0
+    global_train_step_count = 0
+    global_val_step_count = 0
     current_epoch=0
     should_stop = False
     train_start_time = time.perf_counter()
@@ -75,26 +83,34 @@ def train_image_classifer(config: DictConfig):
 
         current_epoch += 1
 
-        global_step_count = train_loop(fabric, 
+        global_train_step_count = train_loop(fabric, 
+                                       train_logger,
                                        model, 
                                        optimizer,
                                        train_dataloader, 
                                        train_start_time, 
                                        current_epoch, 
-                                       global_step_count, 
+                                       global_train_step_count, 
                                        max_steps = config.workload.max_steps,
                                        limit_train_batches = config.workload.limit_train_batches)
         
         if val_dataloader is not None and current_epoch % config.workload.validation_frequency == 0:
-            validate_loop(fabric, model, val_dataloader)
-
+            global_val_step_count=  validate_loop(fabric, 
+                                        val_logger,
+                                       model, 
+                                       val_dataloader,
+                                       train_start_time, 
+                                       current_epoch, 
+                                       global_val_step_count, 
+                                       config.workload.limit_val_batches)
+  
         if current_epoch % config.workload.checkpoint_frequency == 0:
             checkpoint = {'epoch': current_epoch,
                           'model_state_dict': model.state_dict(),
                           'optimizer_state_dict': optimizer.state_dict()}
             fabric.save(os.path.join(config.checkpoint_dir, f"epoch-{current_epoch:04d}.ckpt"), checkpoint)
         
-        if config.workload.max_steps is not None and global_step_count >= config.workload.max_steps:
+        if config.workload.max_steps is not None and global_train_step_count >= config.workload.max_steps:
             should_stop = True
         if config.workload.max_epochs is not None and current_epoch >= config.workload.max_epochs:
             should_stop = True
@@ -138,8 +154,13 @@ def get_transforms(workload_name):
         raise ValueError(f"Invalid workload: {workload_name}")
     return train_transform, val_transform
 
-def train_loop(fabric, model, optimizer, train_dataloader, train_start_time, current_epoch, global_step_count, max_steps = None, limit_train_batches = np.inf, criterion=nn.CrossEntropyLoss()):
+def train_loop(fabric, train_logger:CSVLogger, model, optimizer, train_dataloader, train_start_time, current_epoch, global_step_count, max_steps = None, limit_train_batches = np.inf, criterion=nn.CrossEntropyLoss()):
     model.train()
+
+    total_samples = 0
+    total_train_loss = 0.0
+    correct_preds = 0
+
     end = time.perf_counter()
 
     for batch_idx, (batch, data_fetch_time, transformation_time, cache_hit) in enumerate(train_dataloader):
@@ -165,7 +186,17 @@ def train_loop(fabric, model, optimizer, train_dataloader, train_start_time, cur
         optimizer.zero_grad()
         torch.cuda.synchronize()
         gpu_processing_time = time.perf_counter() - gpu_processing_started
+
+        # Metrics calculation
+        total_train_loss += train_loss * inputs.size(0)  # accumulate total loss
+        correct_preds += (outputs.argmax(dim=1) == labels).sum().item() # accumulate correct predictions
+        total_samples += inputs.size(0)
+
+        # Calculate average loss and accuracy across all batches
+        avg_train_loss = total_train_loss / total_samples
+        avg_train_acc = correct_preds / total_samples
         global_step_count +=1
+
         metrics= OrderedDict({
                         "Elapsed Time (s)": time.perf_counter() - train_start_time,
                         "Device": fabric.global_rank,
@@ -178,14 +209,10 @@ def train_loop(fabric, model, optimizer, train_dataloader, train_start_time, cur
                         "Data Fetch Time (s)": data_fetch_time,
                         "Transformation Time (s)": transformation_time,
                         "Cache Hit/Miss": 1 if cache_hit else 0,
-                        # "cpu (%)": get_cpu_usage(),
-                        # "gpu (%)": get_gpu_usage(),
-                        "Train Loss": train_loss,
-                        "Train Accuracy": train_acc,
-                        "Validation  Loss": 0,
-                        "Validation  Accuracy": 0,
+                        "Avg Train Loss": avg_train_loss, #calculates the average training loss across all batches.
+                        "Avg Train Accuracy": avg_train_acc, #calculates the average training accuracy across all batches.
                         })
-        fabric.log_dict(metrics,step=global_step_count)
+        train_logger.log_metrics(metrics,step=global_step_count)
         
         fabric.print(
                 f"Epoch {metrics['Epoch Index']} ({metrics['Batch Index']}/{min(len(train_dataloader),limit_train_batches)}) |"
@@ -211,13 +238,69 @@ def train_loop(fabric, model, optimizer, train_dataloader, train_start_time, cur
     return  global_step_count
 
 
-def validate_loop(fabric, model, dataloader):
-    # Validation
+def validate_loop(fabric,val_logger:CSVLogger, model, dataloader, val_start_time, current_epoch, global_step_count, limit_val_batches=np.inf, criterion=nn.CrossEntropyLoss()):
     model.eval()
-    for i, batch in enumerate(dataloader):
+    end = time.perf_counter()
+
+    total_val_loss = 0.0
+    correct_preds = 0
+    total_samples = 0
+
+    for batch_idx, (batch, data_fetch_time, transformation_time, cache_hit) in enumerate(dataloader):
+        if batch_idx >= limit_val_batches:
+            break
+        
+        inputs, labels = batch
+
+        # Forward pass
+        gpu_processing_started = time.perf_counter()
         with torch.no_grad():
-            output = model(batch)
-            # Do something with output
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+        gpu_processing_time = time.perf_counter() - gpu_processing_started
+
+        # Metrics calculation
+        val_loss = loss.item()
+        val_acc = (outputs.argmax(dim=1) == labels).float().mean().item()
+        
+        total_val_loss += val_loss * inputs.size(0)  # accumulate total loss
+        correct_preds += (outputs.argmax(dim=1) == labels).sum().item()
+        total_samples += inputs.size(0)
+
+        # Calculate average loss and accuracy
+        avg_val_loss = total_val_loss / total_samples #Calculates the average validation loss across all batches.
+        avg_val_acc = correct_preds / total_samples #Calculates the average validation accuracy across all batches.
+        global_step_count +=1
+
+        metrics = OrderedDict({
+            "Elapsed Time (s)": time.perf_counter() - val_start_time,
+            "Device": fabric.global_rank,
+            "Epoch Index": current_epoch,
+            "Batch Index": batch_idx + 1,
+            "Batch Size": batch[0].size(0),
+            "Total Iteration Time (s)": time.perf_counter() - end,
+            "Data Fetch Time (s)": data_fetch_time,
+            "Transformation Time (s)": transformation_time,
+            "Cache Hit/Miss": 1 if cache_hit else 0,
+            "Avg Validation Loss": avg_val_loss,
+            "Avg Validation Accuracy": avg_val_acc
+        })
+        
+        val_logger.log_metrics(metrics, step=global_step_count)
+
+        fabric.print(
+            f"Epoch {metrics['Epoch Index']} ({metrics['Batch Index']}/{min(len(dataloader), limit_val_batches)}) |"
+            f" iter time: {metrics['Total Iteration Time (s)']:.2f} |"
+            f" dataload time: {metrics['Data Fetch Time (s)']:.2f} |"
+            f" gpu time: {metrics['Data Fetch Time (s)']:.2f} |"
+            f" elapsed time: {metrics['Elapsed Time (s)']:.2f} |"
+            f" val loss: {metrics['Avg Validation Loss']:.3f} |"
+            f" val acc: {metrics['Avg Validation Accuracy']:.3f}"
+        )
+
+        end = time.perf_counter()
+
+    return global_step_count
 
 @hydra.main(version_base=None, config_path="./conf", config_name="config")
 def main(config: DictConfig):
