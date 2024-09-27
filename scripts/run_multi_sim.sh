36a11d6be42849ac4dfb35ec14908c9d788b9c4b
pw-02 <watters.patrick@outlook.com>
2024-09-26 18:45:36 -0700

    name: coordl

--- scripts/run_multi_sim.sh
+++ scripts/run_multi_sim.sh
@@ -2,7 +2,7 @@
 set -e
 
 workload="imagenet_resnet50" # Define your workload imagenet_resnet50, cifar10_resnet18, lora_finetune_owt, albef_retrieval
-dataloder="shade" # Define your dataloader shade, super, litgpt
+dataloder="coordl" # Define your dataloader shade, super, litgpt
 
 # Define an array of GPU indices
 job_ids=(0 1 2 3)
