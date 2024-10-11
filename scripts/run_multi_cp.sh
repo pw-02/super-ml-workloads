05bdb044ab8c11144602e2fe9f7f400853692cfa
pw-02 <watters.patrick@outlook.com>
2024-10-11 09:15:33 -0700

    imagenet_resnet50

--- scripts/run_multi_cp.sh
+++ scripts/run_multi_cp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-workload="cifar10_resnet18" # Define your workload imagenet_resnet50, cifar10_resnet18, lora_finetune_owt, albef_retrieval
+workload="imagenet_resnet50" # Define your workload imagenet_resnet50, cifar10_resnet18, lora_finetune_owt, albef_retrieval
 dataloder="super" # Define your dataloader shade, super, litgpt, coordl
 
 # Define an array of GPU indices
