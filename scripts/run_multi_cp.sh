b643026a390dd7bfa785764cedb4aa664b767185
pw-02 <watters.patrick@outlook.com>
2024-10-14 11:11:20 -0700

    sahde image net

--- scripts/run_multi_cp.sh
+++ scripts/run_multi_cp.sh
@@ -2,7 +2,7 @@
 set -e
 
 workload="imagenet_resnet50" # Define your workload imagenet_resnet50, cifar10_resnet18, lora_finetune_owt, albef_retrieval
-dataloder="super" # Define your dataloader shade, super, litgpt, coordl
+dataloder="shade" # Define your dataloader shade, super, litgpt, coordl
 
 # Define an array of GPU indices
 job_ids=(0 1 2 3)
