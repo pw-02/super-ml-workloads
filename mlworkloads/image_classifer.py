10e914449e3d0ce16d93b1a1dbc91a54ff7b3658
pw-02 <watters.patrick@outlook.com>
2024-10-25 02:42:35 -0700

    simple transform

--- mlworkloads/image_classifer.py
+++ mlworkloads/image_classifer.py
@@ -228,9 +228,9 @@ def get_transforms(workload_name):
         # Set up data transforms for ImageNet
         train_transform = transforms.Compose([
             transforms.Resize(256), 
-            transforms.RandomResizedCrop(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),  # Randomly change brightness, contrast, saturation, and hue
+            # transforms.RandomResizedCrop(224),
+            # transforms.RandomHorizontalFlip(),
+            # transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),  # Randomly change brightness, contrast, saturation, and hue
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
