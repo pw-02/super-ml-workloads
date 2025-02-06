ad826d7479bd404414043c089b34f6065d6d054e
pw-02 <watters.patrick@outlook.com>
2025-02-05 16:23:08 -0800

    lets see

--- mlworkloads/image_classifer.py
+++ mlworkloads/image_classifer.py
@@ -159,7 +159,7 @@ def train_image_classifer(config: DictConfig,  train_logger: CSVLogger, val_logg
     # metric_collector.stop()
 
 def get_transforms(workload_name):
-    if 'imagenet' in workload_name:
+    if 'imagenet' in workload_name or 'cifar10' in workload_name:
         # Set up data transforms for ImageNet
         train_transform = transforms.Compose([
             transforms.Resize(256), 
@@ -176,24 +176,24 @@ def get_transforms(workload_name):
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
+
+    # elif 'cifar10' in workload_name:
+    #       # Set up data transforms for ImageNet
+    #     train_transform = transforms.Compose([
+    #         transforms.Resize(224),
+    #         transforms.RandomHorizontalFlip(),
+    #         transforms.RandomCrop(32, padding=4),
+    #         # transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),  # Randomly change brightness, contrast, saturation, and hue
+    #         # transforms.RandomRotation(15),      # Randomly rotate images by up to 15 degrees
+    #         transforms.ToTensor(),                    # Convert to tensor
+    #         transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])  # Normalize
+    #     ])
         
-    elif 'cifar10' in workload_name:
-          # Set up data transforms for ImageNet
-        train_transform = transforms.Compose([
-            transforms.Resize(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.RandomCrop(32, padding=4),
-            # transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),  # Randomly change brightness, contrast, saturation, and hue
-            # transforms.RandomRotation(15),      # Randomly rotate images by up to 15 degrees
-            transforms.ToTensor(),                    # Convert to tensor
-            transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])  # Normalize
-        ])
-        
-        val_transform = transforms.Compose([
-            transforms.Resize(224),
-            transforms.ToTensor(),                    # Convert to tensor
-            transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])  # Normalize
-       ])        
+    #     val_transform = transforms.Compose([
+    #         transforms.Resize(224),
+    #         transforms.ToTensor(),                    # Convert to tensor
+    #         transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])  # Normalize
+    #    ])        
     else:
         raise ValueError(f"Invalid workload: {workload_name}")
     return train_transform, val_transform
